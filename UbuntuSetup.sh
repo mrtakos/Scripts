c89790c7a286feaabda74f7c16a854d94c1a7862
mrtakos <mrtakos@users.noreply.github.com>
2014-03-30 19:54:49 -0700

    modified:   UbuntuSetup.sh

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-echo Setting up Ubuntu environment...
+echo "Setting up Ubuntu environment..."
 
+echo "add-apt-repository"
+sudo add-apt-repository "deb http://cran.fhcrc.org/bin/linux/ubuntu precise/"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+sudo add-apt-repository "deb http://repo.steampowered.com/steam/ precise"
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
+
 sudo apt-get update
+
+echo "Basic apps"
+sudo apt-get install ubuntu-restricted-extras gnome-terminal gedit 
+sudo apt-get install synaptic dpkg nano chromium-browser
+
+echo "Remote apps"
+sudo apt-get install remmina
+
+echo "Dev packages"
+sudo apt-get install r-base ipython 
 sudo apt-get install sublime-text
 sudo apt-get install pandoc
+
+echo "Game Packages"
+sudo apt-get install wine steam 
